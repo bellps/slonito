0f6625b33f18df0b015dc576cafcdb4ab3049261
bellps <malvezzi.isabella@gmail.com>
2024-09-06 22:17:35 -0300

    fix(gui): adjusts chat scroll

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim,html.slim}',
-    './app/components/*.{erb,haml,html,slim,html.slim}',
+    './app/views/**/*.{erb,haml,html,slim,html.slim,html.erb}',
+    './app/components/*.{erb,haml,html,slim,html.slim,html.erb}',
     './node_modules/flowbite/**/*.js'
   ],
   theme: {
