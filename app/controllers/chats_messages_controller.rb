11509db47725ceb59e75dfbf9033e04255e5a248
bellps <malvezzi.isabella@gmail.com>
2024-09-23 17:41:47 -0300

    feat(chat): generates response from api

--- app/controllers/chats_messages_controller.rb
+++ app/controllers/chats_messages_controller.rb
@@ -8,6 +8,8 @@ class ChatsMessagesController < ApplicationController
     @message.is_input = true
 
     if @message.save
+      generate_response
+
       head :ok
     else
       head :unprocessable_entity
@@ -18,5 +20,30 @@ class ChatsMessagesController < ApplicationController
 
   def chat_message_params
     params.require(:chat_message).permit(:content, :chat_id)
+  end
+
+  def generate_response
+    response = ChatMessage.new(is_input: false, chat_id: @message.chat_id)
+
+    response.content = get_llm_response
+    response.save
+  end
+
+  def get_llm_response
+    response = HTTParty.post(
+      "#{ENV.fetch('SLONITO_API_URL')}/generate",
+      {
+        body: {
+          prompt: @message.content,
+          sql_schema: @message.chat_raw_schema
+        }.to_json,
+        headers: {
+          'Content-Type' => 'application/json'
+        },
+        timeout: 500000
+      }
+    )
+
+    JSON.parse(response.body)['response']
   end
 end
