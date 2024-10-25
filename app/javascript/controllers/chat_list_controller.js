9556cddaabb473f2b3862d4ebb2b1fa2151e3a49
bellps <malvezzi.isabella@gmail.com>
2024-10-25 20:55:41 -0300

    feat(sidebar): adjusts functions order

--- app/javascript/controllers/chat_list_controller.js
+++ app/javascript/controllers/chat_list_controller.js
@@ -4,90 +4,6 @@ import { Collapse } from 'flowbite';
 export default class extends Controller {
   static targets = ["chatItem"]
 
-  connect() {
-    document.addEventListener(
-      "turbo:visit",
-      this.handleTurboEvents.bind(this)
-    );
-
-    document.addEventListener(
-      "turbo:frame-load",
-      this.handleTurboEvents.bind(this)
-    );
-  }
-
-  updateActiveChat() {
-    const chatId = this.getCurrentChatId();
-
-    if (!chatId) return;
-
-    var currentChat = window.FlowbiteInstances.getInstance('Collapse', `dropdown_from_${chatId}`);
-
-    if (!currentChat) return;
-
-    currentChat.expand();
-
-    this.chatItemTargets.forEach(function (chatItem) {
-      var dropdownId = `dropdown_from_${chatItem.dataset.chatId}`
-
-      if (window.FlowbiteInstances.instanceExists('Collapse', dropdownId)) {
-        var chat = window.FlowbiteInstances.getInstance('Collapse', dropdownId)
-
-        if (chat != currentChat) chat.collapse()
-      }
-    })
-  }
-
-  handleTurboEvents() {
-    this.updateActiveChat();
-  }
-
-  disconnect() {
-    document.removeEventListener(
-      "turbo:visit",
-      this.handleTurboEvents.bind(this)
-    );
-    document.removeEventListener(
-      "turbo:frame-load",
-      this.handleTurboEvents.bind(this)
-    );
-  }
-
-  chatItemTargetConnected(chatItem) {
-    var item;
-    var controller = this;
-
-    var dropdownId = `dropdown_from_${chatItem.dataset.chatId}`
-
-    if (!window.FlowbiteInstances.instanceExists('Collapse', dropdownId)) {
-      var dropdown = document.getElementById(dropdownId);
-      var options = {
-        onCollapse: (callback) => {
-          controller.unselectChat(callback)
-        },
-        onExpand: (callback) => {
-          controller.selectChat(callback)
-        }
-      };
-
-      item = new Collapse(dropdown, chatItem, options);
-
-      window.FlowbiteInstances.addInstance('Collapse', item, dropdownId)
-    } else {
-      item = window.FlowbiteInstances.getInstance('Collapse', dropdownId)
-    }
-
-    if (this.getCurrentChatId() == chatItem.dataset.chatId) item.expand();
-  }
-
-  chatItemTargetDisconnected(chatItem) {
-    var dropdownId = `dropdown_from_${chatItem.dataset.chatId}`
-    
-    if (window.FlowbiteInstances.instanceExists('Collapse', dropdownId)) {
-      window.FlowbiteInstances.destroyAndRemoveInstance('Collapse', dropdownId)
-    }
-  }
-
   getCurrentChatId() {
     const currentUrl = window.location.pathname;
     const chatIdMatch = currentUrl.match(/\/chats\/(\d+)/);
@@ -109,5 +25,88 @@ export default class extends Controller {
     const iconEl = callback._triggerEl.querySelector(".arrow-icon");
 
     iconEl.classList.remove('rotate-180')
+  }
+
+  chatItemTargetConnected(chatItem) {
+    var item;
+    const dropdownId = `dropdown_from_${chatItem.dataset.chatId}`
+
+    if (!window.FlowbiteInstances.instanceExists('Collapse', dropdownId)) {
+      const controller = this;
+      const options = {
+        onCollapse: (callback) => {
+          controller.unselectChat(callback)
+        },
+        onExpand: (callback) => {
+          controller.selectChat(callback)
+        }
+      };
+
+      item = new Collapse(document.getElementById(dropdownId), chatItem, options);
+
+      window.FlowbiteInstances.addInstance('Collapse', item, dropdownId)
+    } else {
+      item = window.FlowbiteInstances.getInstance('Collapse', dropdownId)
+    }
+
+    if (this.getCurrentChatId() == chatItem.dataset.chatId) item.expand();
+  }
+
+  chatItemTargetDisconnected(chatItem) {
+    const dropdownId = `dropdown_from_${chatItem.dataset.chatId}`;
+    
+    if (window.FlowbiteInstances.instanceExists('Collapse', dropdownId)) {
+      window.FlowbiteInstances.destroyAndRemoveInstance('Collapse', dropdownId);
+    }
+  }
+
+  updateActiveChat() {
+    const chatId = this.getCurrentChatId();
+
+    if (!chatId) return;
+
+    const currentChat = window.FlowbiteInstances.getInstance('Collapse', `dropdown_from_${chatId}`);
+
+    if (!currentChat) return;
+
+    currentChat.expand();
+
+    this.chatItemTargets.forEach(function (chatItem) {
+      var dropdownId = `dropdown_from_${chatItem.dataset.chatId}`;
+
+      if (window.FlowbiteInstances.instanceExists('Collapse', dropdownId)) {
+        var chat = window.FlowbiteInstances.getInstance('Collapse', dropdownId);
+
+        if (chat != currentChat) chat.collapse();
+      }
+    })
+  }
+
+  handleTurboEvents() {
+    this.updateActiveChat();
+  }
+
+  connect() {
+    document.addEventListener(
+      "turbo:visit",
+      this.handleTurboEvents.bind(this)
+    );
+
+    document.addEventListener(
+      "turbo:frame-load",
+      this.handleTurboEvents.bind(this)
+    );
+  }
+
+  disconnect() {
+    document.removeEventListener(
+      "turbo:visit",
+      this.handleTurboEvents.bind(this)
+    );
+
+    document.removeEventListener(
+      "turbo:frame-load",
+      this.handleTurboEvents.bind(this)
+    );
   }
 }
